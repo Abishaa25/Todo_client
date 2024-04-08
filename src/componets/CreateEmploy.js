bb380059e720e74e142f4b0704587ffc1c298773
Abishaa25 <110649945+Abishaa25@users.noreply.github.com>
2024-04-08 12:16:19 +0530

    Add files via upload

--- src/componets/CreateEmploy.js
+++ src/componets/CreateEmploy.js
@@ -1,89 +1,125 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function CreateEmploy() {
     const history = useNavigate();
-  const [input, setInput] = useState({
-    id:"",
-    name: "",
-    mail: "",
-    phone: "",
-    role: "",
-  });
+    const [input, setInput] = useState({
+      id:"",
+      name: "",
+      mail: "",
+      phone: "",
+      role: "",
+    });
 
 
   function submitHandler(e) {
     e.preventDefault();
-
-    axios
+    const form = e.currentTarget;
+    if (!form.checkValidity()) {
+      toast.warning('Please fill the required field', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      e.stopPropagation();
+    } else {
+      axios
       .post("http://localhost:8000/employe", input)
       .then((res) => {
-        alert("Employe Data Added Successfully");
-        history("/");
+        toast.success('Employee Data Updated successfully', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          history('/');
+        },2000)
+        
       })
       .catch((err) => {
         console.log(err);
       });
+    }    
   }
-
   return (
     <div className=" bg-dark vh-100 d-flex justify-content-center">
-      <form onSubmit={submitHandler} className="container-lg bg-secondary py-5 px-5 w-50 m-5 rounded border border-info">
-      <div class="form-group">
-          <label for="name">Enter Id:</label>
+      <Form onSubmit={submitHandler} className="container-lg bg-secondary py-5 px-5 w-50 m-5 rounded border border-info" noValidate>
+      <div className ="form-group">
+          <label htmlFor="name">Enter Id:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className ="form-control my-2"
             id="name"
             placeholder="Enter your Id"
             onChange={(e) => setInput({ ...input, id: e.target.value })}
+            required
           />
+          <div className="invalid-feedback">Please enter the Employee Id</div>
         </div>
-        <div class="form-group">
-          <label for="name">Name:</label>
+        <div className ="form-group">
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className ="form-control my-2"
             id="name"
             placeholder="Enter your name"
             onChange={(e) => setInput({ ...input, name: e.target.value })}
+            required
           />
+          <div className="invalid-feedback">Please enter the name.</div>
         </div>
-        <div class="form-group">
-          <label for="email">Email :</label>
+        <div className ="form-group">
+          <label htmlFor="email">Email :</label>
           <input
             type="text"
-            class="form-control my-2"
+            className ="form-control my-2"
             id="email"
             placeholder="Enter email"
             onChange={(e) => setInput({ ...input, mail: e.target.value })}
+            required
           />
+          <div className="invalid-feedback">Please enter the name.</div>
         </div>
-        <div class="form-group">
-          <label for="email">Phone No:</label>
+        <div className ="form-group">
+          <label htmlFor="email">Phone No:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className ="form-control my-2"
             id="email"
             placeholder="Enter phone No"
             onChange={(e) => setInput({ ...input, phone: e.target.value })}
+            required
           />
+          <div className="invalid-feedback">Please enter the name.</div>
         </div>
-        <div class="form-group">
-          <label for="email">Role:</label>
+        <div className ="form-group">
+          <label htmlFor="email">Role:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className ="form-control my-2"
             id="email"
             placeholder="Enter Role"
             onChange={(e) => setInput({ ...input, role: e.target.value })}
           />
         </div>
-        <button type="submit" class="btn btn-primary my-4">
+        <button type="submit" className ="btn btn-primary my-4">
           Submit
         </button>
-      </form>
+      </Form>
+      <ToastContainer />
+
     </div>
   );
 }
--- src/componets/UpdateEmploye.js
+++ src/componets/UpdateEmploye.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 
 function UpdateEmploye() {
   const id = useParams().id;
   const history=useNavigate()
-  console.log(id);
   const [datas, setData] = useState([]);
-
   useEffect(() => {
     axios
       .get("http://localhost:8000/employe/" + id)
@@ -22,8 +22,19 @@ function UpdateEmploye() {
   function handelSubmit(event) {
     event.preventDefault();
     axios.put("http://localhost:8000/employe/" + id, datas).then((res) => {
-      alert("Employee Data Updated Successfully")
-      history('/')
+      // alert("Employee Data Updated Successfully")
+      toast.success('Employee Data Updated successfully', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setTimeout(() => {
+        history('/');
+      },2000)
      
     }).catch((err)=>{console.log(err);})
   }
@@ -33,60 +44,61 @@ function UpdateEmploye() {
     <div className=" bg-dark vh-100 d-flex justify-content-center">
         
       <form onSubmit={handelSubmit} className="container-lg bg-secondary py-5 w-50 m-5 rounded px-5">
-        <div class="form-group">
-          <label for="name">Enter Id:</label>
+        <div className="form-group">
+          <label htmlFor="name">Enter Id:</label>
           <input
             type="text"
-            class="form-control my-2"
-            id="name"
+            className="form-control my-2"
+            id="name" disabled
             value={datas.id}
            
           />
         </div>
-        <div class="form-group">
-          <label for="name">Enter Name:</label>
+        <div className="form-group">
+          <label htmlFor="name">Enter Name:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className="form-control my-2"
             id="name"
             value={datas.name}
             onChange={(e) => setData({ ...datas, name: e.target.value })}
           />
         </div>
-        <div class="form-group">
-          <label for="name">Enter Email:</label>
+        <div className="form-group">
+          <label htmlFor="name">Enter Email:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className ="form-control my-2"
             id="name"
             value={datas.mail}
             onChange={(e) => setData({ ...datas, mail: e.target.value })}
           />
         </div>
-        <div class="form-group">
-          <label for="email"> Phone No:</label>
+        <div className="form-group">
+          <label htmlFor="email"> Phone No:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className="form-control my-2"
             id="email"
             value={datas.phone}
             onChange={(e) => setData({ ...datas, phone: e.target.value })}
           />
         </div>
-        <div class="form-group">
-          <label for="email">Email Role:</label>
+        <div className="form-group">
+          <label htmlFor="email">Email Role:</label>
           <input
             type="text"
-            class="form-control my-2"
+            className="form-control my-2"
             id="email"
             value={datas.role}
             onChange={(e) => setData({ ...datas, role: e.target.value })}
           />
         </div>
-        <button type="submit" class="btn btn-primary my-4">
+        <button type="submit" className="btn btn-primary my-4">
           Update
         </button>
       </form>
+      <ToastContainer />
     </div>
   );
 }
